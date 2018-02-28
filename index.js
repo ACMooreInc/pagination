bcb00b91eb700cdec19010350012cb54270454bc
ACMOORE\rclipper <rclipper@acmoore.com>
2018-02-28 14:51:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+exports.pagination = function (req, res, next) {
+  if (req.query.page_size && req.query.page_size.match(/^\d+$/)) {
+    req.pagination = {
+      page_number: req.query.page_number ? req.query.page_number : 1,
+      page_size: req.query.page_size
+    };
+  }
+  next();
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Pagination Middleware
+
+Middleware to parse pagination properties set as query parameters.
+
+## To Use
+```
+require('acm-pagination');
+```
