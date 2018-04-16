ea238c01fb5ccdf85e94e46742a48ea4ada2b097
Ari M <amesyn@users.noreply.github.com>
2018-04-16 12:17:12 -0400

    Move pagination object into req.params

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 exports.pagination = function (req, res, next) {
   if (req.query.page_size && req.query.page_size.match(/^\d+$/)) {
-    req.pagination = {
+    req.params = req.params || {};
+    req.params.pagination = {
       page_number: req.query.page_number ? req.query.page_number : 1,
       page_size: req.query.page_size
     };
